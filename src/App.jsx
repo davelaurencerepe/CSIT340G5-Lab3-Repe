9090eee64514f32fa210b1ba7f7ef277074cd0d3
Laurencesoi <daverepe7@gmail.com>
2026-09-21 22:13:45 +0800

    1.1: course information step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
-const App = () => {
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of units {props.total}</p>
+}
+
+const Footer = (props) => {
+  return (
+    <p>
+      {props.fullName} - {props.courseCode} - {props.section}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = 'Web Systems and Technologies'
+  const part1 = 'Data Structures and Algorithms'
+  const exercises1 = 3
+  const part2 = 'Object-Oriented Programming'
+  const exercises2 = 3
+  const part3 = 'Discrete Mathematics'
+  const exercises3 = 3
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+      <Footer fullName='Dave Laurence R. Repe' courseCode='CSIT340' section='G5' />
     </div>
   )
 }
